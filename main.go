1c7b4b23740bb319f54083041011b4d13093cf25
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-07-25 17:37:13 +0530

    project layout

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found")
+	}
+
+	fmt.Println("PORT: ", portString)
+}
