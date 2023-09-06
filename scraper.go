912f3b502cffffccf93d72731df8ad1a98a9c638
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-09-06 20:27:04 +0530

    creating a post logic

--- scraper.go
+++ scraper.go
@@ -50,8 +50,35 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
   }
 
   for _, item := range rssFeed.Channel.Item {
-    log.Println("Saving item: ", item.Title)
+    description := sql.NullString{}
+    if item.Description != "" {
+      description.String = item.Description
+      description.Valid = true
+    }
+
+    pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+    if err != nil {
+      log.Printf("Could not parse data %v with error %v", item.PubDate, err)
+      continue
+    }
+
+    _, err = db.CreatePost(context.Background(), 
+      database.CreatePostParams{
+        ID: uuid.New(),
+        CreatedAt: time.Now().UTC(),
+        UpdatedAt: time.Now().UTC(),
+        Title: item.Title,
+        Description: description,
+        PublishedAt: pubAt,
+        Url: item.Link,
+        FeedID: feed.ID,
+      }
+      )
+    if err != nil {
+      log.Printf("Error creating post: %v", err)
+    }
   }
+
 
   log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 
